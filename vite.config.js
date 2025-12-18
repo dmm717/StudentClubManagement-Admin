f4c3eb1e13e84babdf7fcfe32a7dadf43cc8afe1
huy <lagiahuy70@gmail.com>
2025-12-19 03:43:39 +0700

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -20,20 +20,11 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('antd')) {
-              if (id.includes('antd/es/table') || id.includes('antd/es/table/')) {
-                return 'antd-table';
-              }
-              if (id.includes('antd/es/form') || id.includes('antd/es/form/')) {
-                return 'antd-form';
-              }
-              if (id.includes('antd/es/date-picker') || id.includes('antd/es/date-picker/')) {
-                return 'antd-datepicker';
-              }
-              return 'antd';
-            }
             if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
               return 'vendor-react';
+            }
+            if (id.includes('antd')) {
+              return 'antd';
             }
             if (id.includes('@microsoft/signalr')) {
               return 'signalr';
@@ -44,17 +35,10 @@ export default defineConfig({
             if (id.includes('sweetalert2')) {
               return 'vendor-sweetalert';
             }
-            if (id.includes('@heroicons')) {
-              return 'vendor-icons';
-            }
-            if (id.includes('dayjs')) {
-              return 'vendor-dayjs';
-            }
-            return 'vendor-other';
           }
         }
       }
     },
-    chunkSizeWarningLimit: 500
+    chunkSizeWarningLimit: 600
   }
 })
